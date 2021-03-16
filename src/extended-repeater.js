715a8474b86c3b0d54186b4647306dcc0ad696fa
vera <poletaeva.vera.vladimirovna@gmail.com>
2021-03-16 20:12:25 +0300

    feat: half basic js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let res = '';
+  let count = options.repeatTimes || 0;
+  let addStr = options.addition ? options.addition.toString() : '';
+  let separator = options.separator ? options.separator.toString() : '+';
+  let addRepeatTimes = options.additionRepeatTimes || 0;
+  let addSeparator = options.additionSeparator ? options.additionSeparator : '';
+  
+  if (typeof str === 'object' && typeof options.addition === 'object') {
+    addStr = `${options.addition}`;
+    str = `${str}`;
+  }
+  
+  if (options.addition == false) {
+    addStr = 'false';
+  }
+  
+  if (options.addition == null) {
+    addStr = 'null';
+  }
+  
+  if (options.addition && (!options.additionSeparator || options.additionSeparator === '') && options.additionRepeatTimes > 1) {
+    addSeparator = '|';
+  }
+  
+  if (count === 0) {
+    return str+addStr;
+  }
+  
+  if (count === 1 && !addStr && !separator) {
+    return str;
+  }
+  
+  if (count === 2 && !addRepeatTimes && !addSeparator) {
+    return str+addStr+separator+str+addStr;
+  }
+  
+  for (let i=0;i<count; i++) {
+    res = res + str;
+    for (let j=0;j<addRepeatTimes; j++) {
+      res = res+addStr;
+      if (j+1 !== addRepeatTimes) {
+        res = res + addSeparator;
+      }
+    }
+    if (i+1 !== count) {
+      res = res + separator;
+    }
+  }
+  return res;
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, lvl = 1) {
+    let max = lvl;
+    for (let i = 0; i < arr.length; i++) {
+      const el = arr[i];
+      if (Array.isArray(el)) {
+        const deep = this.calculateDepth(el, lvl + 1);
+        if (deep > max) {
+          max = deep;
+        }
+      }
+    }
+    return max;
   }
 };
