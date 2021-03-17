79b47b79a9903bdf71037dc18679b441ac5eba61
vera <poletaeva.vera.vladimirovna@gmail.com>
2021-03-17 21:59:42 +0300

    feat: half basic js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,44 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-const chainMaker = {
+class ChainMaker {
+  res = [];
+  
   getLength() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
   }
-};
+  
+  addLink(value) {
+    this.res.push(value && value.toString());
+    return this;
+  }
+  
+  removeLink(position) {
+    if (typeof position !== 'number') {
+      this.res = [];
+      throw new Error('THROWN');
+    }
+    
+    const index = position - 1;
+    if (index < 0 || index > this.res.length) {
+      this.res = [];
+      throw new Error('THROWN');
+    }
+    
+    this.res.splice(index, 1);
+    return this;
+  }
+  
+  reverseChain() {
+    this.res = this.res.reverse();
+    return this;
+  }
+  
+  finishChain() {
+    const result = this.res.map((value) => value === undefined ? '( )' : `( ${value} )`).join('~~');
+    this.res = [];
+    return result;
+  }
+}
 
-module.exports = chainMaker;
+module.exports = new ChainMaker();
